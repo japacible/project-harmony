0e4f619d6f26e204174e8b73d715489d19f889af
David Mailhot <djmailhot@gmail.com>
2012-11-19 15:50:44 -0800

    Adding first itty bit of js code to ui prototype

---
+++ js/ui_prototype.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function() {
+	$("#home-btn").click(function() { alert("go home") });
+	$("#conversation-btn").click(function() { alert("have a conversation") });
+	$("#playback-btn").click(function() { alert("enter playback mode") });
+	$("#exit-btn").click(function() { window.location.href = "index.html#prototype" });
+
+});
