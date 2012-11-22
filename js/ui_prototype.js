b88514b3450eb7f22d96cde32726c300a3375e72
David Mailhot <djmailhot@gmail.com>
2012-11-21 22:40:52 -0800

    3 Distinct screens (main, home, playback).  Navigation buttons now move between them.

--- js/ui_prototype.js
+++ js/ui_prototype.js
@@ -1,8 +1,27 @@
 
-$(document).ready(function() {
-	$("#home-btn").click(function() { alert("go home") });
-	$("#conversation-btn").click(function() { alert("have a conversation") });
-	$("#playback-btn").click(function() { alert("enter playback mode") });
-	$("#exit-btn").click(function() { window.location.href = "index.html#prototype" });
 
+$(document).ready(function() {
+	// prep main screen buttons
+	$("#screen-main .home-btn").click(function(){ changeScreenTo("screen-home"); });
+	$("#screen-main .friends-btn").click(function(){ alert("clicked"); });
+	$("#screen-main .record-btn").click(function(){ alert("clicked"); });
+	$("#screen-main .feedback-btn").click(function(){ alert("clicked"); });
+
+	// prep home screen buttons
+	$("#screen-home .main-btn").click(function() { changeScreenTo("screen-main"); });
+	$("#screen-home .playback-btn").click(function() { changeScreenTo("screen-playback"); });
+	$("#screen-home .settings-btn").click(function() { alert("clicked"); });
+	$("#screen-home .exit-btn").click(function() { window.location.href = "index.html#prototype" });
+
+	// prep playback screen buttons
+	$("#screen-playback .back-btn").click(function() { changeScreenTo("screen-home"); });
+	$("#screen-playback .play-btn").click(function() { alert("clicked"); });
 });
+
+
+
+
+function changeScreenTo(screenName) {
+	$(".displaying").removeClass("displaying");
+	$("#"+screenName).addClass("displaying");
+}
