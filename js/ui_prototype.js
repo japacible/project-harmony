6b2e8cf9d51172052012a6f55c6a1d5a8aa3e66b
David Mailhot <djmailhot@gmail.com>
2012-11-29 11:32:42 -0800

    Compressed screens to 2 total, removed settings,  25% overlay

--- js/ui_prototype.js
+++ js/ui_prototype.js
@@ -3,32 +3,20 @@
 $(document).ready(function() {
 
 	// prep main screen buttons
-	$('#screen-main .home-btn').click(function(){ changeScreenTo('screen-home'); });
-	$('#screen-main .friends-btn').click(function(){ alert('clicked'); });
-	$('#screen-main .record-btn').click(function(){ alert('clicked'); });
-	$('#screen-main .feedback-btn').click(function(){ alert('clicked'); });
-
-
-	// prep home screen buttons
-	$('#screen-home .main-btn').click(function() { changeScreenTo('screen-main'); });
-	$('#screen-home .playback-btn').click(function() { changeScreenTo('screen-playback'); });
-	$('#screen-home .settings-btn').click(function() { alert('clicked'); });
-	$('#screen-home .exit-btn').click(function() { window.location.href = 'index.html#prototype' });
+	$('#playback-btn').click(function() { changeScreenTo('screen-playback'); });
+	$('#friends-btn').click(function(){ alert('clicked'); });
+	$('#record-btn').click(function(){ alert('clicked'); });
+	$('#feedback-btn').click(function(){ alert('clicked'); });
+	$('#exit-btn').click(function() { window.location.href = 'index.html#prototype' });
 
 
 	// prep playback screen buttons
-	$('#screen-playback .back-btn').click(function() { changeScreenTo('screen-home'); });
-	$('#screen-playback .previous-btn').click(function() { alert('clicked'); });
-	$('#screen-playback .start-btn').click(function() { changeScreenTo('screen-viewvideo'); });
-	$('#screen-playback .next-btn').click(function() { alert('clicked'); });
-
-
-	// prep viewvideo screen buttons
-	$('#screen-viewvideo .back-btn').click(function() { changeScreenTo('screen-playback'); });
-	$('#screen-viewvideo .previous-btn').click(function() { alert('clicked'); });
-	$('#screen-viewvideo .play-btn').click(function() { flipPlayButton(this) });
-	$('#screen-viewvideo .next-btn').click(function() { alert('clicked'); });
-
+	$('#backmain-btn').click(function() { changeScreenTo('screen-main'); });
+	$('#previousvideo-btn').click(function() { alert('clicked'); });
+	$('#rewind-btn').click(function() { alert('clicked'); });
+	$('#play-btn').click(function() { flipPlayButton(this) });
+	$('#fastforward-btn').click(function() { alert('clicked'); });
+	$('#nextvideo-btn').click(function() { alert('clicked'); });
 
 });
 
